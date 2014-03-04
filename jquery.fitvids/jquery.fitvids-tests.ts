c27d51707727e4ce2b85a210a8d56da28e61cdd3
Peter Gill <peter.gill@townsuite.com>
2014-03-04 12:00:30 -0330

    Add definition for fitVids.
    See https://github.com/davatron5000/FitVids.js.

---
+++ jquery.fitvids/jquery.fitvids-tests.ts
@@ -0,0 +1,5 @@
+/// <reference path="../jquery/jquery.d.ts"/>
+/// <reference path="jquery.placeholder.d.ts"/>
+
+$("#content").fitVids();
+$("#content").fitVids({ customSelector: "iframe[src^='http://mycoolvideosite.com'], iframe[src^='http://myviiids.com']"});
---
+++ jquery.fitvids/jquery.fitvids.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for jquery.fitvids.js 1.1
+// Project: https://github.com/davatron5000/FitVids.js
+// Definitions by: Peter Gill <https://github.com/majorsilence>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../jquery/jquery.d.ts"/>
+
+interface JQuery {
+
+	fitVids();
+	fitVids(options: any);
+
+}
